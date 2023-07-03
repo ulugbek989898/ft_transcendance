c0f3404f0cbb924d40601afe1e1c66bbd0dc3392
ulugbek989898 <47236469+ulugbek989898@users.noreply.github.com>
2023-07-03 12:01:53 +0400

    first commit

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,5 @@
+export const HomePage = () => {
+	return (
+		<div>Hello world from HomePage!</div>
+	);
+}
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/LoginPage.style.css"
+
+export default function LoginPage () {
+	const url = process.env.REACT_APP_API_URL;
+
+	return (
+		<div className="main">
+			<a href={url} className="link-redirect">Login from 42</a>
+		</div>
+	);
+}
