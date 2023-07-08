d22cb659d4194fd5be78f5760649a646a8d10aa8
Ulugbek Isroilov <uisroilo@lab2r1s8.42abudhabi.ae>
2023-07-08 17:09:07 +0400

    added .env

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "../styles/LoginPage.style.css"
 
 export default function LoginPage () {
@@ -6,7 +5,6 @@ export default function LoginPage () {
 	return (
 		<div className="main">
 			<a href={url} className="link-redirect" >Login from 42</a>
-			{/* <a href={url} className="text-2xl inline-block py-3 px-5 " >Login from 42</a> */}
 		</div>
 	);
 }
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -10,7 +10,7 @@ export default function ProfilePage() {
 			<>
 				<Header />
 				<PersonalInfo />
-				{/* <HorizontalLine /> */}
+				<HorizontalLine />
 				<Tabs />
 			</>
 		</Container>
